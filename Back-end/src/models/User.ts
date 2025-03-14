fcfcef904a8b3c2ef95c56fb77bb33167b3142dc
MhdRayanBinSN <rayan6203@gmail.com>
2025-03-14 16:25:54 +0530

    crcted mistake of model controller

---
+++ Back-end/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type: String,
+        require:[true, 'Full Name is required']
+    },email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+      },phone: {
+        type: String,
+        required: [true, 'Phone number is required'],
+        match: [/^[0-9]{10}$/, 'Please enter a valid 10-digit phone number']
+      },
+      password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters long'],
+        select: false // Excludes password by default in queries
+      }
+
+},{
+    timestamps: true
+})
+
+export const User =  mongoose.model('User',userSchema);
